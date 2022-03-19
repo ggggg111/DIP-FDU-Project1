40dc4b00fd6dee65cefbdf69cc4f50eefe7c3482
nngg1104 <nngg1104@gmail.com>
2022-03-19 01:34:02 +0100

    First HDR test

--- ImageEditor/ImageEditor/src/tools/HDRLoader.cpp
+++ ImageEditor/ImageEditor/src/tools/HDRLoader.cpp
@@ -1,3 +1,7 @@
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <opencv2/photo.hpp>
+
 #include "HDRLoader.h"
 
 void HDRLoader::LoadHDRImage(const std::vector<std::string>& image_paths, const std::vector<float>& exposure_times)
@@ -5,7 +9,17 @@ void HDRLoader::LoadHDRImage(const std::vector<std::string>& image_paths, const 
 	std::vector<cv::Mat> images = HDRLoader::ReadImages(image_paths);
 	std::vector<float> times = ReadTimes(exposure_times);
 
+	cv::Ptr<cv::AlignMTB> align_mtb = cv::createAlignMTB();
+	align_mtb->process(images, images);
 
+	cv::Mat response_debevec;
+	cv::Ptr<cv::CalibrateDebevec> calibrateDebevec = cv::createCalibrateDebevec();
+	calibrateDebevec->process(images, response_debevec, times);
+
+	cv::Mat hdr_debevec;
+	cv::Ptr<cv::MergeDebevec> merge_debevec = cv::createMergeDebevec();
+	merge_debevec->process(images, hdr_debevec, times, response_debevec);
+	cv::imwrite("image.hdr", hdr_debevec);
 }
 
 std::vector<cv::Mat> HDRLoader::ReadImages(const std::vector<std::string>& image_paths)
--- ImageEditor/ImageEditor/src/tools/HDRLoader.h
+++ ImageEditor/ImageEditor/src/tools/HDRLoader.h
@@ -5,8 +5,6 @@
 #include <string>
 
 #include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
 
 class HDRLoader
 {
