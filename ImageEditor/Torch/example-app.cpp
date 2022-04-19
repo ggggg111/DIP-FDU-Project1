deaf82108a1d7fb06a2aadd3c7d1acd0797162cd
nngg1104 <nngg1104@gmail.com>
2022-04-19 08:37:27 +0200

    Fixed postbuild command

--- ImageEditor/Torch/example-app.cpp
+++ ImageEditor/Torch/example-app.cpp
@@ -1,18 +1,22 @@
-#include <torch/torch.h>
 #include <iostream>
+#include <torch/torch.h>
 
-int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  if (torch::cuda::is_available()) {
-	  std::cout << "CUDA is available! Training on GPU" << std::endl;
-	  auto tensor_cuda = tensor.cuda();
-	  std::cout << tensor_cuda << std::endl;
-  }
-  else
-  {
-	  std::cout << "CUDA is not available! Training on CPU" << std::endl;
-	  std::cout << tensor << std::endl;	  
-  }
+int main()
+{
+	torch::Tensor tensor = torch::rand({2, 3});
+	
+	if (torch::cuda::is_available())
+	{
+		std::cout << "CUDA is available! Training on GPU" << std::endl;
+		auto tensor_cuda = tensor.cuda();
+		std::cout << tensor_cuda << std::endl;
+	}
+	else
+	{
+		std::cout << "CUDA is not available! Training on CPU" << std::endl;
+		std::cout << tensor << std::endl;	  
+	}
 
-  std::cin.get();
+	std::cin.get();
+	return 0;
 }
