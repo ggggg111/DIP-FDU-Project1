c6701263298221a89ce0e40fe9061f7d3f8132bc
nngg1104 <nngg1104@gmail.com>
2022-04-23 00:45:30 +0200

    Getting anomaly map from output

--- ImageEditor/Torch/example-app.cpp
+++ ImageEditor/Torch/example-app.cpp
@@ -7,43 +7,43 @@
 
 int main()
 {
-	/*torch::Tensor tensor = torch::rand({2, 3});
-	
-	if (torch::cuda::is_available())
-	{
-		std::cout << "CUDA is available! Training on GPU" << std::endl;
-		auto tensor_cuda = tensor.cuda();
-		std::cout << tensor_cuda << std::endl;
-	}
-	else
-	{
-		std::cout << "CUDA is not available! Training on CPU" << std::endl;
-		std::cout << tensor << std::endl;	  
-	}
-
-	std::cin.get();
-	return 0;*/
-
-	const char* encoder_path = "serialized_fastflow.zip";
-	torch::jit::script::Module encoder;
+	const char* model_path = "serialized_fastflow.zip";
+	torch::jit::script::Module model;
 	try
 	{
-		encoder = torch::jit::load(encoder_path);
+		model = torch::jit::load(model_path);
 		std::cout << "Model loaded correctly" << std::endl;
 	}
 	catch (const c10::Error& e)
 	{
 		std::cout << "Model loaded incorrectly" << std::endl;
-		std::cerr << "error loading the model\n";
+		std::cerr << "error loading the model" << std::endl;
 		return -1;
 	}
-	encoder.eval();
+	model.to(torch::kCUDA);
+	//model.eval();
 
 	cv::Mat img = cv::imread("002.png", cv::IMREAD_COLOR);
 	std::cout << img.size << " x " << img.channels() << std::endl;
 
+	cv::resize(img, img, cv::Size(256, 256));
+
 	at::Tensor tensor_image = torch::from_blob(img.data, { img.rows, img.cols, img.channels() }, at::kByte);
 	std::cout << tensor_image.sizes() << std::endl;
+
+	tensor_image = tensor_image.to(at::kFloat).div(255).unsqueeze(0);
+	std::cout << tensor_image.sizes() << std::endl;
+	tensor_image = tensor_image.permute({ 0, 3, 1, 2 });
+	std::cout << tensor_image.sizes() << std::endl;
+	tensor_image.sub_(0.5).div_(0.5);
+	tensor_image = tensor_image.to(torch::kCUDA);
+
+	c10::IValue output = model.forward({ tensor_image });
+	c10::Dict<at::IValue, at::IValue> out = output.toGenericDict();
+	at::Tensor anomaly_map = out.at("anomaly_map").toTensor();
+	std::cout << anomaly_map.sizes() << std::endl;
+	
+	std::cout << "Done!\n";
 
 	//std::vector<torch::jit::IValue> inputs;
 	//inputs.push_back(torch::ones({ 1, 3, 224, 224 }));
